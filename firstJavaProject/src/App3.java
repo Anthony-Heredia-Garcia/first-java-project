8e496f27cc4f2e23b443521434654f108d1ae745
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-10-15 21:38:49 -0400

    new file app3

--- firstJavaProject/src/App3.java
+++ firstJavaProject/src/App3.java
@@ -1,22 +1,7 @@
 
-import java.util.Scanner;
-
 public class App3 {
   public static void main(String[] args) {
-    double x;
-    double y;
-    double z;
-
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter side X: ");
-    x = scanner.nextDouble();
-
-    System.out.println("Enter side y: ");
-    y = scanner.nextDouble();
-
-    z = Math.sqrt((x*x)+(y*y));
-
-    System.out.println("The hypotenuse is: "+z);
-    scanner.close();
+    
+    
   }
 }
